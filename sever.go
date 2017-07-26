80a201ceefdf0e3fb9775f304080f8f479a89faa
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-07-26 13:11:51 +0900

    sql.DB have connection pool. so you don't need to re-open DB each times.

--- sever.go
+++ sever.go
@@ -17,11 +17,15 @@ type Data struct {
 	Humidity    float64 `json:"humidity"`
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
+type DB struct {
+	sql.DB
+}
+
+func (db *DB) show(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
+func (db *DB) post(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
@@ -32,29 +36,22 @@ func post(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-	db(datas)
+	if err := db.insert(datas); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
 }
 
-func db(datas Data) {
-	db, err := sql.Open("sqlite3", "sensor.sqlite3")
+func (db *DB) insert(datas Data) error {
+	_, err := db.Exec("insert into sensor(temperature, humidity, timestanp) values(" + fmt.Sprint(datas.Temperature) + ", " + fmt.Sprint(datas.Humidity) + ", \"" + fmt.Sprint(time.Now()) + "\");")
 	if err != nil {
-		fmt.Println(err)
-	}
-	// fmt.Println("insert into sensor values(" +  fmt.Sprint(datas.Temperature) + ", " +  fmt.Sprint(datas.Humidity) + ", \"" +  fmt.Sprint(time.Now()) + "\");")
-	_, err = db.Exec("insert into sensor(temperature, humidity, timestanp) values(" + fmt.Sprint(datas.Temperature) + ", " + fmt.Sprint(datas.Humidity) + ", \"" + fmt.Sprint(time.Now()) + "\");")
-	if err != nil {
-		fmt.Println(err)
-	}
-	db.Close()
-}
-
-func prepare() error {
-	db, err := sql.Open("sqlite3", "sensor.sqlite3")
-	if err != nil {
-		fmt.Println(err)
 		return err
 	}
-	_, err = db.Exec(
+	return nil
+}
+
+func (db *DB) prepare() error {
+	_, err := db.Exec(
 		`create table if not exists sensor(id integer primary key, temperature REAL , humidity REAL, timestanp text)`,
 	)
 	return err
@@ -64,12 +61,18 @@ func prepare() error {
 //  curl http://localhost:8080/post -X POST -d "{"temperature":1.2,"humidity":3.3}"
 
 func main() {
-	if err := prepare(); err != nil {
+	db, err := sql.Open("sqlite3", "sensor.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn := &DB{*db}
+
+	if err := conn.prepare(); err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/post", post)
+	http.HandleFunc("/", conn.show)
+	http.HandleFunc("/post", conn.post)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
